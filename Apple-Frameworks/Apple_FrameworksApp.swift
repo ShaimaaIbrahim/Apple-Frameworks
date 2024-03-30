a750e3469bb97c9f78589a0f2e3c9f540b8411ca
platinumwireless <shaimaa@sulfah.com>
2024-03-30 20:53:38 +0200

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Platinum WireLess on 30/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
